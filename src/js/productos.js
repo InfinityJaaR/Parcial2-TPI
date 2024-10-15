dde4c4d40f95aab26717b2701b121350392a59fb
Emerson Arévalo <AO22002@ues.edu.sv>
2024-10-14 20:37:20 -0600

    Dando formato a los objetos JSON

--- src/js/productos.js
+++ src/js/productos.js
@@ -1,11 +1,46 @@
 const productos = [
-  { nombre: 'Bocadillos', precio: 10.00, imagen: '../src/img/queiques/bocadillos.png', descripcion: 'Deliciosos bocadillos variados.' },
-  { nombre: 'Cheesecake', precio: 20.00, imagen: '../src/img/queiques/chesecake.png', descripcion: 'Suave y cremoso cheesecake.' },
-  { nombre: 'Pan Dulce', precio: 30.00, imagen: '../src/img/queiques/pan-dulce.png', descripcion: 'Tradicional pan dulce esponjoso.' },
-  { nombre: 'Pastel Capuccino', precio: 40.00, imagen: '../src/img/queiques/pastel-capucino.png', descripcion: 'Exquisito pastel con sabor a capuccino.' },
-  { nombre: 'Pastel de Chocolate', precio: 50.00, imagen: '../src/img/queiques/pastel-chocolate.png', descripcion: 'Irresistible pastel de chocolate.' },
-  { nombre: 'Pastel Personalizado', precio: 60.00, imagen: '../src/img/queiques/personalizado.png', descripcion: 'Personaliza tu pastel a tu gusto.' },
-  { nombre: 'Torta', precio: 60.00, imagen: '../src/img/queiques/torta.png', descripcion: 'Clásica torta para toda ocasión.' }
+  { 
+    nombre: 'Bocadillos', 
+    precio: 10.00, 
+    imagen: '../src/img/queiques/bocadillos.png', 
+    descripcion: 'Deliciosos bocadillos variados.'
+  },
+  { 
+    nombre: 'Cheesecake', 
+    precio: 20.00, 
+    imagen: '../src/img/queiques/chesecake.png', 
+    descripcion: 'Suave y cremoso cheesecake.' 
+  },
+  { 
+    nombre: 'Pan Dulce', 
+    precio: 30.00, 
+    imagen: '../src/img/queiques/pan-dulce.png', 
+    descripcion: 'Tradicional pan dulce esponjoso.' 
+  },
+  { 
+    nombre: 'Pastel Capuccino', 
+    precio: 40.00, 
+    imagen: '../src/img/queiques/pastel-capucino.png', 
+    descripcion: 'Exquisito pastel con sabor a capuccino.' 
+  },
+  { 
+    nombre: 'Pastel de Chocolate', 
+    precio: 50.00, 
+    imagen: '../src/img/queiques/pastel-chocolate.png', 
+    descripcion: 'Irresistible pastel de chocolate.' 
+  },
+  { 
+    nombre: 'Pastel Personalizado', 
+    precio: 60.00, 
+    imagen: '../src/img/queiques/personalizado.png', 
+    descripcion: 'Personaliza tu pastel a tu gusto.' 
+  },
+  { 
+    nombre: 'Torta', 
+    precio: 60.00, 
+    imagen: '../src/img/queiques/torta.png', 
+    descripcion: 'Clásica torta para toda ocasión.' 
+  }
 ];
 
   // Función para generar la lista de productos
